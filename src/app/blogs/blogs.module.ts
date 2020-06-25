5d2013456efe1940cd701dd17ec18e8bc7bf4b11
Shubham <shubhamsinha2050@gmail.com>
2020-06-25 16:24:29 +0530

    Added blogs module to handle the articles

---
+++ src/app/blogs/blogs.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ArticleListComponent } from './article-list/article-list.component';
+import { ArticleDetailComponent } from './article-detail/article-detail.component';
+import { CommentsComponent } from './comments/comments.component';
+import { BlogsComponent } from './blogs/blogs.component';
+
+
+
+@NgModule({
+  declarations: [ArticleListComponent, ArticleDetailComponent, CommentsComponent, BlogsComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class BlogsModule { }
