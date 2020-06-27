a92d2f140aa9d811b1d018a52540af317e3449b3
Bhubanesh Pinani <shubhamsinha2050@gmail.com>
2020-06-27 11:47:25 +0530

    Added the blogs detail page

--- src/app/blogs/blogs-routing.module.ts
+++ src/app/blogs/blogs-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ArticleNewComponent } from './article-new/article-new.component';
 import { ArticleListComponent } from './article-list/article-list.component';
+import { BlogsComponent } from './blogs.component';
 
 const routes: Routes = [
     {path: 'blog/new', component: ArticleNewComponent},
     {path: 'blog/list', component: ArticleListComponent},
+    {path: 'blog/selected', component: BlogsComponent},
 ];
 
 @NgModule({
