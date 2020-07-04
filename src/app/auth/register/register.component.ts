e9026c119fcc5dd6288494c105c361bd0dd1e258
Bhubanesh Pinani <shubhamsinha2050@gmail.com>
2020-07-04 18:39:05 +0530

    Added registration validation

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
 import { debounceTime } from 'rxjs/operators';
 import { AuthService } from 'src/app/services/auth.service';
+import { LoggerService } from 'src/app/services/cx-menu/realtimelogger.service';
 
 // function passwordMatch(c: AbstractControl): {[key: string]: boolean} | null{
 //   const p1 = c.get('password');
@@ -17,6 +18,23 @@ import { AuthService } from 'src/app/services/auth.service';
 //   return {'match': true};
 // }
 
+// function emaiValid(c: AbstractControl): { [key: string]: boolean } | null {
+//   var em = c.get("email").value;
+//   var emr = em.split('.');
+
+//   if (emr[emr.length - 1] == '.com') {
+//     console.log("well");
+//     return null
+//     // return {'valid': false}
+//   }
+//   if (c.get("email").pristine) {
+//     return null;
+//   }
+//   console.log("returning")
+//   return { 'value': false };
+//   //return {'valid':true}
+// }
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -27,52 +45,63 @@ export class RegisterComponent implements OnInit {
   registrationForm: FormGroup;
   hide: boolean = true;
   user: {};
-  disableButton:boolean = false
+  disableButton: boolean = false
   registrationDone: boolean = false;
-  errorMessage: string ;
+  errorMessage: string;
+  error;
+  response;
   constructor(private fb: FormBuilder,
-    private authService: AuthService) { }
+    private authService: AuthService,
+    private loggerService: LoggerService) { }
 
 
   ngOnInit() {
     this.registrationForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email,]],
+      email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(10)]],
       // passwordGroup: this.fb.group({
       //   password: ['', [Validators.required]],
       //   confirm_password: ['', [Validators.required]]
       // }, {validators: passwordMatch}),
-      full_name: ['', [Validators.required, ]],
-    })
+      full_name: ['', [Validators.required,]],
+    },)
 
     console.log(this.registrationForm);
 
     this.registrationForm.get('email').valueChanges.pipe(
       debounceTime(1000)
     ).subscribe(
-      value => console.log(value),
+      value => { }
+      // console.log(value),
     )
+
+    this.loggerService.logData("auth-register", this);
   }
 
-  register(){
+  register() {
     this.disableButton = true;
     console.log("Registered the user");
     console.log(this.registrationForm);
     this.user = {};
     this.user["email"] = this.registrationForm.value["email"];
     this.user["first_name"] = this.registrationForm.value["full_name"].split(" ")[0];
-    this.user["last_name"] = this.registrationForm.value["full_name"].replace(this.user["first_name"]+" ", '').replace(this.user["first_name"], '');
+    this.user["last_name"] = this.registrationForm.value["full_name"].replace(this.user["first_name"] + " ", '').replace(this.user["first_name"], '');
     this.user["password"] = this.registrationForm.value["password"];
     this.authService.register_root_user(this.user).subscribe(
-      response =>{
+      response => {
         this.registrationDone = true;
         this.disableButton = true;
+        this.response = response;
         console.log(response);
-      }, 
+      },
       error => {
         // console.log(error.error["message"]);
-        this.errorMessage = error.error["message"];
-        
+        if (error.error["ue"]==true){
+          this.errorMessage = error.error["message"];
+        }
+        this.errorMessage = "The email address is invalid"
+        // this.errorMessage = error.error;
+        this.error = error;
         this.disableButton = false;
       }
     )
