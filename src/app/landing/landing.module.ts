2342f3c72e70d6ca638756e448e64cb111c7e6ca
Bhubanesh Pinani <shubhamsinha2050@gmail.com>
2020-07-05 23:52:22 +0530

    Added all-articles

--- src/app/landing/landing.module.ts
+++ src/app/landing/landing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LandingComponent } from './landing.component';
+import { AllArticlesComponent } from './all-articles/all-articles.component';
 
 
 
 @NgModule({
-  declarations: [LandingComponent],
+  declarations: [LandingComponent, AllArticlesComponent],
   imports: [
     CommonModule
   ]
