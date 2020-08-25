9beb849618249aeba02bf66bf986270cdd5ac94c
Bhubanesh Pinani <shubhamsinha2050@gmail.com>
2020-08-25 16:18:56 +0530

    Added footer

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-footer',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  subscribeForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+  ) { 
+    
+  }
 
   ngOnInit() {
+
+    this.subscribeForm = this.fb.group({
+      email: ['', [Validators.required, Validators.minLength(1), Validators.email]],
+      // confirm_password: ['', [Validators.required, Validators.minLength(8)]]
+    })
+  }
+
+  subscribeViaMail(){
+    // console.log( this.subscribeForm.get("email").value);
+    //make the service to send it to the mailing list
   }
 
 }
