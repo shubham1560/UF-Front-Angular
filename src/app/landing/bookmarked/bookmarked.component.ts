f7f9b823f1d4080572160901bbfdd376ddfea2ec
Bhubanesh Pinani <shubhamsinha2050@gmail.com>
2020-09-06 03:02:13 +0530

    Added bookmark functionality

--- src/app/landing/bookmarked/bookmarked.component.ts
+++ src/app/landing/bookmarked/bookmarked.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { LoggerService } from 'src/app/services/cx-menu/realtimelogger.service';
 import { UserprofileService } from 'src/app/services/userprofile/userprofile.service';
+import { MatDialog, MAT_DIALOG_DATA } from '@angular/material';
 
 @Component({
   selector: 'app-bookmarked',
@@ -11,17 +12,35 @@ export class BookmarkedComponent implements OnInit {
 
   constructor(
     private loggerService: LoggerService,
-    private userService: UserprofileService
+    private userService: UserprofileService,
+    public dialog: MatDialog,
+    @Inject(MAT_DIALOG_DATA) public data: any
   ) { }
   bookmarkedArticles;
+  start = 0;
+  end = 3;
+  showButton = true;
+  title = "Recently Bookmarked";
   ngOnInit(): void {
-    this.userService.getBookmarkedArticles(0, 3).subscribe(
+    if (this.data["modal"]){
+      this.end = 0;
+      this.showButton = false;
+      this.title = "All Bookmarked Articles"
+    }
+    this.userService.getBookmarkedArticles(this.start, this.end).subscribe(
       (result:any) =>{
-        this.bookmarkedArticles = result;
-
+        this.bookmarkedArticles = result.bookmarked_articles;
       }
     )
     this.loggerService.logData("uf-bookmarked", this);
   }
 
+
+  openAllBookmarkedArticles(){
+    this.dialog.open(BookmarkedComponent, 
+      {
+        data: {"modal": true}
+      }
+      );
+  }
 }
--- src/app/landing/bookmarked/dist/bookmarked.component.js
+++ src/app/landing/bookmarked/dist/bookmarked.component.js
@@ -5,27 +5,50 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
 exports.__esModule = true;
 exports.BookmarkedComponent = void 0;
 var core_1 = require("@angular/core");
+var material_1 = require("@angular/material");
 var BookmarkedComponent = /** @class */ (function () {
-    function BookmarkedComponent(loggerService, userService) {
+    function BookmarkedComponent(loggerService, userService, dialog, data) {
         this.loggerService = loggerService;
         this.userService = userService;
+        this.dialog = dialog;
+        this.data = data;
+        this.start = 0;
+        this.end = 3;
+        this.showButton = true;
+        this.title = "Recently Bookmarked";
     }
+    BookmarkedComponent_1 = BookmarkedComponent;
     BookmarkedComponent.prototype.ngOnInit = function () {
         var _this = this;
-        this.userService.getBookmarkedArticles(0, 3).subscribe(function (result) {
-            _this.bookmarkedArticles = result;
+        if (this.data["modal"]) {
+            this.end = 0;
+            this.showButton = false;
+            this.title = "All Bookmarked Articles";
+        }
+        this.userService.getBookmarkedArticles(this.start, this.end).subscribe(function (result) {
+            _this.bookmarkedArticles = result.bookmarked_articles;
         });
         this.loggerService.logData("uf-bookmarked", this);
     };
-    BookmarkedComponent = __decorate([
+    BookmarkedComponent.prototype.openAllBookmarkedArticles = function () {
+        this.dialog.open(BookmarkedComponent_1, {
+            data: { "modal": true }
+        });
+    };
+    var BookmarkedComponent_1;
+    BookmarkedComponent = BookmarkedComponent_1 = __decorate([
         core_1.Component({
             selector: 'app-bookmarked',
             templateUrl: './bookmarked.component.html',
             styleUrls: ['./bookmarked.component.scss']
-        })
+        }),
+        __param(3, core_1.Inject(material_1.MAT_DIALOG_DATA))
     ], BookmarkedComponent);
     return BookmarkedComponent;
 }());
