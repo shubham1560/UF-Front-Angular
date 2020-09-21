76ef3c05e2b058a3de9585edd71e52cde46cb871
Bhubanesh Pinani <shubhamsinha2050@gmail.com>
2020-09-21 16:25:51 +0530

    Added page for all the components

--- src/app/about/about.module.ts
+++ src/app/about/about.module.ts
@@ -6,10 +6,11 @@ import { WhoWeAreComponent } from './who-we-are/who-we-are.component';
 import { WhatMattersComponent } from './what-matters/what-matters.component';
 import { WhatWeDoComponent } from './what-we-do/what-we-do.component';
 import { HowWeDoItComponent } from './how-we-do-it/how-we-do-it.component';
+import { AboutUsComponent } from './about-us.component';
 
 
 @NgModule({
-  declarations: [WhoWeAreComponent, WhatMattersComponent, WhatWeDoComponent, HowWeDoItComponent],
+  declarations: [WhoWeAreComponent, WhatMattersComponent, WhatWeDoComponent, HowWeDoItComponent, AboutUsComponent],
   imports: [
     CommonModule,
     AboutRoutingModule
