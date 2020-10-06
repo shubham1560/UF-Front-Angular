dcf63d8cb0c1323f77be42074f98edbddb8b269b
Bhubanesh Pinani <shubhamsinha2050@gmail.com>
2020-10-06 10:31:59 +0530

    Added style for user profile

--- src/app/userprofile/authored-articles/authored-articles.component.ts
+++ src/app/userprofile/authored-articles/authored-articles.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserprofileService } from 'src/app/services/userprofile/userprofile.service';
 import { state } from '@angular/animations';
+import { LoggerService } from 'src/app/services/cx-menu/realtimelogger.service';
 
 @Component({
   selector: 'app-authored-articles',
@@ -11,6 +12,7 @@ export class AuthoredArticlesComponent implements OnInit {
 
   constructor(
     private userProfile: UserprofileService,
+    private loggerService: LoggerService,
   ) { }
 
 
@@ -18,46 +20,51 @@ export class AuthoredArticlesComponent implements OnInit {
   sort_by = "sys_created_on";
   all_articles;
   state = 'all';
-  isLoading =false;
+  isLoading = false;
   // start = 0;
   ngOnInit(): void {
     this.userProfile.getUserAuthoredArticles(this.sort_by, this.state).subscribe(
-      (response:any)=>{
+      (response: any) => {
         // this.all_articles = response
         this.articles_data = response;
         this.isLoading = false;
       }
     )
+
+    this.loggerService.logData("uf-authored-articles", this);
+
   }
 
   // filterByState(state){
   //   this.articles_data = this.all_articles[0]
   // }  
 
-  changeState(state_to_change: string){
+  changeState(state_to_change: string) {
     this.state = state_to_change;
     this.isLoading = true;
-    this.ngOnInit();  
+    this.ngOnInit();
     // console.log("called")
     // console.log(this);
   }
 
-  changeSort(sort: string){
-    this.isLoading = true;
-    if (sort == this.sort_by){
-      if(sort[0]=="-"){
-      this.sort_by = sort.substring(1, sort.length)
+  changeSort(sort: string) {
+    if (this.articles_data.articles.length > 1) {
+      this.isLoading = true;
+      if (sort == this.sort_by) {
+        if (sort[0] == "-") {
+          this.sort_by = sort.substring(1, sort.length)
+        }
+        else {
+          this.sort_by = "-" + sort;
+        }
+      } else {
+        this.sort_by = sort;
       }
-      else{
-        this.sort_by = "-"+sort;
+      if (sort == 'workflow') {
+        this.sort_by = 'workflow';
       }
-    }else{
-      this.sort_by = sort;
+      this.ngOnInit();
     }
-    if(sort == 'workflow'){
-      this.sort_by = 'workflow';
-    }
-    this.ngOnInit();
   }
 
 }
