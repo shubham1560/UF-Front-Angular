bce0062c10a972a61e3800be5a03e107ae8160ed
Bhubanesh Pinani <shubhamsinha2050@gmail.com>
2020-11-08 23:28:24 +0530

    Added user info component to the user module

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UsersRoutingModule } from './users-routing.module';
+import { UserInfoComponent } from './user-info/user-info.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [UserInfoComponent],
   imports: [
     CommonModule,
     UsersRoutingModule
