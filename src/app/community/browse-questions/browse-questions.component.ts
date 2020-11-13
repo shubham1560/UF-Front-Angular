093ccb9732eed4208de0d4925c579506b3efeabc
Bhubanesh Pinani <shubhamsinha2050@gmail.com>
2020-11-14 01:45:40 +0530

    Added get request

--- src/app/community/browse-questions/browse-questions.component.ts
+++ src/app/community/browse-questions/browse-questions.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CommunityService } from '../../services/community/community.service'
 
 @Component({
   selector: 'app-browse-questions',
@@ -9,13 +10,25 @@ import { ActivatedRoute } from '@angular/router';
 export class BrowseQuestionsComponent implements OnInit {
 
   constructor(
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private commService: CommunityService
   ) { }
+
+  questions;
 
   ngOnInit(): void {
     this.route.queryParamMap.subscribe(
       params=>{
         // console.log(params);
+        var root = params.get('root')
+        var path = params.get('path');
+        var article = params.get('article');
+        this.commService.getQuestions(root, path, article, 0, 5).subscribe(
+          result=>{
+            console.log(result);
+            this.questions = result;
+          }
+        )
       }
     )
   }
--- src/app/services/community/community.service.ts
+++ src/app/services/community/community.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { AuthService } from '../authservice/auth.service';
+import { UrlconfigService } from '../urlconfig.service';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommunityService {
 
-  constructor() { }
+  constructor(
+    private auth: AuthService,
+    private urlService: UrlconfigService,
+    private httpService: HttpClient
+  ) { }
+
+  base_url = this.urlService.getUrl();
+
+  base_community_url = `${this.base_url}community/`;
+
+  called_url = ''
+
+  getHeader() {
+    return this.auth.getHeader();
+  }
+
+  getQuestions(root, path, article, start, end) {
+    this.called_url = `${this.base_community_url}question/get?root=${root}&path=${path}&article=${article}&start=${start}&end=${end}`;
+    return this.httpService.get(this.called_url, { headers : this.getHeader()});
+  }
 }
