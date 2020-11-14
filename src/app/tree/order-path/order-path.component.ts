8e7cc6fe7edcba5d11061878b272a1bb509a76aa
Bhubanesh Pinani <shubhamsinha2050@gmail.com>
2020-11-14 16:25:34 +0530

    Added order root

--- src/app/tree/order-path/order-path.component.ts
+++ src/app/tree/order-path/order-path.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/services/knowledgeservice/knowledge.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import { LoggerService } from 'src/app/services/cx-menu/realtimelogger.service';
 
 @Component({
   selector: 'app-order-path',
@@ -6,10 +10,73 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order-path.component.scss']
 })
 export class OrderPathComponent implements OnInit {
+  constructor(
+    private knowledge: DataService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private loggerService: LoggerService
+  ) { }
 
-  constructor() { }
 
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.paths?.categories, event.previousIndex, event.currentIndex);
+  }
+
+  roots;
+  loading = false;
+  paths
+  articles
   ngOnInit(): void {
+    this.route.queryParamMap.subscribe(
+      params => {
+        if (params.get('root')) {
+          this.knowledge.getRelatedCategories(params.get('root'), 'root', 'course').subscribe(
+            result => {
+              this.paths = result;
+            }
+          )
+        }
+        else {
+          this.paths = [];
+        }
+      }
+    )
+    this.loggerService.logData("st-order-root", this);
+  }
+
+  fetchData() {
+    this.loading = true;
+    this.getBases();
+  }
+
+  getBases() {
+    this.knowledge.getKnowledgeBases().subscribe(
+      result => {
+        this.loading = false;
+        this.roots = result;
+      },
+      error => {
+        this.loading = false;
+      }
+    )
+  }
+
+  finalArray = []
+
+  addOrder() {
+    var order = 100
+    this.finalArray = [];
+    this.paths.categories.forEach(element => {
+      // console.log(element);
+      element.order = order;
+      order += 100;
+      this.finalArray.push({
+        "id": element.id,
+        "order": element.order,
+        "label": element.label
+      })
+    });
+    console.log(this.finalArray);
   }
 
 }
--- src/app/tree/tree-routing.module.ts
+++ src/app/tree/tree-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RootComponent } from './root/root.component';
+import { OrderPathComponent } from './order-path/order-path.component';
+import { AuthGuard } from '../auth/guard/auth.guard';
 
 
 const routes: Routes = [
   {
     path: '',
     component: RootComponent
+  },
+  {
+    path:'path_order',
+    component: OrderPathComponent
   },
   {
     path: ':kb_base/:kb_category',
